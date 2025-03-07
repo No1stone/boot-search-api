2aa7f362e3454e87c1ba8e661090f0b0a007e4fb
jangwonseok <jangws1003@naver.com>
2025-03-07 10:09:34 +0900

    유튭 추가

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ pricing = https://www.microsoft.com/en-us/bing/apis/pricing
 azure ref = https://portal.azure.com/#home
 이건 지금 오류나서 애저 문의중
 
+youtube Data Api v3
 
+ref = https://console.cloud.google.com/
 
+![img_1.png](src/main/resources/static/img_1.png)
 
+![img.png](img.png)
+
+video id를 사용하면됨.
+https://www.youtube.com/watch?v={videoId}
+![img_2.png](src/main/resources/static/img_2.png)
--- src/main/java/api/search/api/bootsearchapi/controller/TestController.java
+++ src/main/java/api/search/api/bootsearchapi/controller/TestController.java
@@ -2,6 +2,7 @@ package api.search.api.bootsearchapi.controller;
 
 import api.search.api.bootsearchapi.service.DuckduckGoService;
 import api.search.api.bootsearchapi.service.GoogleSearchApiService;
+import api.search.api.bootsearchapi.service.YoutubeDataSearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class TestController {
 
     private final GoogleSearchApiService googleSearchApiService;
     private final DuckduckGoService duckduckGoService;
+    private final YoutubeDataSearchService youtubeDataSearchService;
 
     @GetMapping("/google/{text}")
     public Map<String, Object> test1(@PathVariable String text) {
@@ -28,4 +30,8 @@ public class TestController {
         return duckduckGoService.search(text);
     }
 
+    @GetMapping("/youtube/{text}")
+    public Map<String, Object> test3(@PathVariable String text) {
+        return youtubeDataSearchService.search(text);
+    }
 }
